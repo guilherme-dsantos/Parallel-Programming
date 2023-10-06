3a7c683c3c1125d5265432d4a528d0bbc167eef6
Guilherme Santos <72032143+guilherme-dsantos@users.noreply.github.com>
2023-10-06 21:03:09 +0100

    knapsack maybe done

--- KnapsackGA/src/knapsack/Main.java
+++ KnapsackGA/src/knapsack/Main.java
@@ -2,6 +2,7 @@ package knapsack;
 
 public class Main {
 	public static void main(String[] args) {
+		// ParallelKnapsackGA ga = new ParallelKnapsackGA();
 		KnapsackGA ga = new KnapsackGA();
 		ga.run();
 	}
--- KnapsackGA/src/knapsack/ParallelKnapsackGA.java
+++ KnapsackGA/src/knapsack/ParallelKnapsackGA.java
@@ -2,17 +2,20 @@ package knapsack;
 
 import java.util.Random;
 
-public class KnapsackGA {
+public class ParallelKnapsackGA {
 	private static final int N_GENERATIONS = 500;
 	private static final int POP_SIZE = 100000;
 	private static final double PROB_MUTATION = 0.5;
 	private static final int TOURNAMENT_SIZE = 3;
+	int nCores = Runtime.getRuntime().availableProcessors();
+	int chunkSize = POP_SIZE / nCores;
+	Thread[] threads = new Thread[nCores];
 
 	private Random r = new Random();
 
 	private Individual[] population = new Individual[POP_SIZE];
 
-	public KnapsackGA() {
+	public ParallelKnapsackGA() {
 		populateInitialPopulationRandomly();
 	}
 
@@ -21,6 +24,7 @@ public class KnapsackGA {
 		for (int i = 0; i < POP_SIZE; i++) {
 			population[i] = Individual.createRandom(r);
 		}
+
 	}
 
 	public void run() {
@@ -28,8 +32,23 @@ public class KnapsackGA {
 		for (int generation = 0; generation < N_GENERATIONS; generation++) {
 
 			// Step1 - Calculate Fitness
-			for (int i = 0; i < POP_SIZE; i++) {
-				population[i].measureFitness();
+			for (int t = 0; t < nCores; t++) {
+				int start = chunkSize * t;
+				int end = (t < nCores - 1) ? chunkSize * (t + 1) : POP_SIZE;
+				threads[t] = new Thread(() -> {
+					for (int i = start; i < end; i++) {
+						population[i].measureFitness();
+					}
+				});
+				threads[t].start();
+			}
+
+			for (int i = 0; i < nCores; i++) {
+				try {
+					threads[i].join();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 			}
 
 			// Step2 - Print the best individual so far.
@@ -41,24 +60,55 @@ public class KnapsackGA {
 			Individual[] newPopulation = new Individual[POP_SIZE];
 			newPopulation[0] = best; // The best individual remains
 
-			for (int i = 1; i < POP_SIZE; i++) {
-				// We select two parents, using a tournament.
-				Individual parent1 = tournament(TOURNAMENT_SIZE, r);
-				Individual parent2 = tournament(TOURNAMENT_SIZE, r);
+			for (int t = 0; t < nCores; t++) {
+				int start = (chunkSize * t == 0) ? 1 : chunkSize * t;
+				int end = (t < nCores - 1) ? chunkSize * (t + 1) : POP_SIZE;
+				threads[t] = new Thread(() -> {
+					for (int i = start; i < end; i++) {
+						// We select two parents, using a tournament.
+						Individual parent1 = tournament(TOURNAMENT_SIZE, r);
+						Individual parent2 = tournament(TOURNAMENT_SIZE, r);
 
-				newPopulation[i] = parent1.crossoverWith(parent2, r);
+						newPopulation[i] = parent1.crossoverWith(parent2, r);
+					}
+				});
+				threads[t].start();
+			}
+
+			for (int i = 0; i < nCores; i++) {
+				try {
+					threads[i].join();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 			}
 
 			// Step4 - Mutate
-			for (int i = 1; i < POP_SIZE; i++) {
-				if (r.nextDouble() < PROB_MUTATION) {
-					newPopulation[i].mutate(r);
+			for (int t = 0; t < nCores; t++) {
+				int start = (chunkSize * t == 0) ? 1 : chunkSize * t;
+				int end = (t < nCores - 1) ? chunkSize * (t + 1) : POP_SIZE;
+				threads[t] = new Thread(() -> {
+					for (int i = start; i < end; i++) {
+						if (r.nextDouble() < PROB_MUTATION) {
+							newPopulation[i].mutate(r);
+						}
+					}
+				});
+				threads[t].start();
+			}
+
+			for (int i = 0; i < nCores; i++) {
+				try {
+					threads[i].join();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
 				}
 			}
+
 			population = newPopulation;
 		}
 		long estimatedTime = System.nanoTime() - startTime;
-		System.out.println("[Sequential] Finished in " + (double) estimatedTime / 1_000_000_000 + " seconds");
+		System.out.println("[Parallel] Finished in " + (double) estimatedTime / 1_000_000_000 + " seconds");
 	}
 
 	private Individual tournament(int tournamentSize, Random r) {
