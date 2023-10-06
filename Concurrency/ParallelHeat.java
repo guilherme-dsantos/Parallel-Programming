0024bc995a3edd49b85ebe5050672a795f1e3081
Guilherme Santos <72032143+guilherme-dsantos@users.noreply.github.com>
2023-10-06 13:07:30 +0100

    Update ParallelHeat.java

--- Concurrency/ParallelHeat.java
+++ Concurrency/ParallelHeat.java
@@ -6,7 +6,7 @@ public class ParallelHeat {
 	public static final int ITERATIONS = 50;
 	private double[][] oldm = new double[NX][NY];
 	private double[][] newm = new double[NX][NY];
-	int nCores = 8;
+	int nCores = Runtime.getRuntime().availableProcessors() - 1;
 	Thread[] threads = new Thread[nCores];
 	int chunkSize = NX / nCores;
 
@@ -29,7 +29,7 @@ public class ParallelHeat {
 		for (int ti = 0; ti < nCores; ti++) {
 			int start = chunkSize * ti + 1;
 			int end = (ti < nCores - 1) ? chunkSize * (ti + 1) : NX - 1;
-
+			System.out.println("START: " + start + " END: " + end);
 			phaser.register();
 
 			threads[ti] = new Thread(() -> {
@@ -37,7 +37,9 @@ public class ParallelHeat {
 				do {
 					double[][] t_oldm = oldm;
 					double[][] t_newm = newm;
+
 					for (int i = start; i < end; i++) {
+
 						for (int j = 1; j < NY - 1; j++) {
 
 							t_newm[i][j] = (t_oldm[i][j] + t_oldm[i - 1][j] + t_oldm[i + 1][j] + t_oldm[i][j - 1]
